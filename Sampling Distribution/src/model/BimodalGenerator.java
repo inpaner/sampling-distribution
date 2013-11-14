5b3df8e07fd384569ad9c7ae73712fc0dce59e79
Ivan Paner <ivan.paner@gmail.com>
2013-11-14 10:16:21 +0800

    Implemented BimodalGenerator

---
+++ Sampling Distribution/src/model/BimodalGenerator.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.TreeMap;
+
+
+public class BimodalGenerator extends AbstractGenerator {
+    protected double mean2;
+    protected double variance2;
+    protected BimodalGenerator(int lowerLimit, int upperLimit, int n) {
+        super(lowerLimit, upperLimit, n);
+        
+        int range = upperLimit - lowerLimit;
+        mean = rand.nextInt(range) + lowerLimit;
+        mean2 = rand.nextInt(range) + lowerLimit;
+        variance2 = rand.nextInt(range) + rand.nextGaussian();
+        
+    }
+
+    @Override
+    int getNext() {
+        double meanToUse = mean;
+        double varToUse = variance;
+        
+        if (rand.nextInt(2) == 0) {
+            meanToUse = mean2;
+            varToUse = variance2;
+        }
+        
+        double stddev = Math.sqrt(varToUse);
+        double retval = Math.max(lowerLimit, Math.min(upperLimit, (int) meanToUse + rand.nextGaussian() * stddev));
+        return (int) retval;
+    }
+    
+
+}
